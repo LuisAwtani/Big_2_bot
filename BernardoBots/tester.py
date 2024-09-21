fa27af0903707774ca3f10160adbf4342fde4f0f
Luis Awtani <luisbernardo.a23@gmail.com>
2024-09-21 13:13:33 +0800

    moved my bots

--- BernardoBots/tester.py
+++ BernardoBots/tester.py
@@ -28,15 +28,15 @@ def findFlushes(hand):
     return flushes
 
 # Function that counts number of stronger 5 card tricks that the player DIDN'T HAVE including card x
-def DisproverFives(cardsPLayed: list[str], CardsInPLayFunc: list[str]):
+def DisproverFives(cardsPLayedAfterNoAnswer: list[str], CardsInPLayFunc: list[str], CardInvestigated: str, handSize: int):
     DisproofCounter = 0
     
     # Disprove Flushes
-    CardsInPLayFunc = CardsInPLayFunc + cardsPLayed
+    CardsInPLayFunc = CardsInPLayFunc + cardsPLayedAfterNoAnswer
     NoOfCardsInPlay = len(CardsInPLayFunc)
     
     print(CardsInPLayFunc)
-    LastFewIndeces = [(NoOfCardsInPlay -1 - i) for i in range(len(cardsPLayed))]
+    LastFewIndeces = [(NoOfCardsInPlay -1 - i) for i in range(len(cardsPLayedAfterNoAnswer))]
     #print(f"Last few indices are: {LastFewIndeces}")
 
     for i in range(NoOfCardsInPlay):
@@ -47,25 +47,35 @@ def DisproverFives(cardsPLayed: list[str], CardsInPLayFunc: list[str]):
                         #print(f"Checking case: {[CardsInPLayFunc[i], CardsInPLayFunc[j], CardsInPLayFunc[k], CardsInPLayFunc[l], CardsInPLayFunc[m]]}")
                         # Disprove Flushes
                         if CardsInPLayFunc[i][1] == CardsInPLayFunc[m][1] and CardsInPLayFunc[i][1] == CardsInPLayFunc[j][1] and CardsInPLayFunc[i][1] == CardsInPLayFunc[k][1] and CardsInPLayFunc[i][1] == CardsInPLayFunc[l][1]:
-                            if i in LastFewIndeces or j in LastFewIndeces or k in LastFewIndeces or l in LastFewIndeces or m in LastFewIndeces:
+                            if CardInvestigated in {CardsInPLayFunc[i],CardsInPLayFunc[j], CardsInPLayFunc[k], CardsInPLayFunc[l], CardsInPLayFunc[m]}:
+                                #print(f"Disproved 1 FLush! If Player held {[CardsInPLayFunc[i], CardsInPLayFunc[j], CardsInPLayFunc[k], CardsInPLayFunc[l], CardsInPLayFunc[m]]}")
                                 DisproofCounter += 1
-                                print(f"Disproved 1 FLush! If Player held {[CardsInPLayFunc[i], CardsInPLayFunc[j], CardsInPLayFunc[k], CardsInPLayFunc[l], CardsInPLayFunc[m]]}")
+    if handSize > 5:
+
+        DisproofCounter = DisproofCounter * PossibleArrangements(NoOfCardsInPlay-5-len(cardsPLayedAfterNoAnswer), handSize-5)
+ 
+    print(f"Successfully disproved {DisproofCounter} scenarios where player held card {CardToInvestigate}")
     return DisproofCounter
 
-
-x = 13
+# y is size of player's hand
 y = 6
 justPlayed = ['9C']
 CardsInPlay = ['2C', 'QC', 'TH', '8H', '8C', '8D', '7H', '7C', '7D', '4C', '5D', '4S', '4D']
 simplifiedCardsInPlay = ['2C', 'QC', '8C', '7C']
 #print(f" Number of cards in play is {len(CardsInPlay)}")
-ScardsInGame = [S(x) for x in CardsInPlay]
+
+#x is quantity of cards still in the game
+x = len(CardsInPlay)
+print(f"There is {x} cards in play")
 
 prb1 = PossibleArrangements(x, y)
-prb2 = PossibleArrangements(x-1, y-1)
-#print(prb1)
-#print(prb2)
-#print(y / x)
-prb2 = prb2 - 10
-#print(prb2 / prb1)
-DisproverFives(justPlayed, CardsInPlay)
+prb2 = PossibleArrangements(x-1, y-1) 
+CardToInvestigate = CardsInPlay[4]
+print(f"Total possible combinations that the player could hold: {prb1}")
+print(f"Total possible combinations that include card {CardToInvestigate}: {prb2}")
+
+Disprovals = DisproverFives(justPlayed, CardsInPlay, CardToInvestigate, y)
+prb2 -= Disprovals
+
+print(y / x)
+print(prb2 / prb1)
